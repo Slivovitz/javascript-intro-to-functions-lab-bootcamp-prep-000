ba01c3ae3934ec3661e229049b86005665df5e76
Yosef Margolin <yukele@hotmail.com>
2017-10-30 05:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
     return("YES INDEED!")
   }
   if (string.toLowerCase() === "hello") {
-    return("I can\'t hear you!")
+    return("I can't hear you!")
   }
   if (string === "I love you, Grandma.") {
     return("I love you, too.")
